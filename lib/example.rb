de1262abac819c124c3259edd176dbf241330f8a
kyle molidor <k_molidor@yahoo.com>
2020-05-11 03:57:50 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,4 @@
 # Use this space to code along with the readme
-def say_hello_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  
+def greeting
+  puts "Hello World"
 end
